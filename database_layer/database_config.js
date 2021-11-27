07f22b5cb71d7b5a7460fdfe8354a2b1d05f8308
SharjeelBaig0508 <baig.sharjeel@yahoo.com>
2021-11-27 19:58:17 +0500

    changed names

--- database_layer/database_config.js
+++ database_layer/database_config.js
@@ -7,5 +7,5 @@ MONGO_PASS = process.env.MONGO_PASS;
 MONGO_DB = process.env.MONGO_DB;
 
 module.exports = {
-  mongodb_url: `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@dream-finder-cluster.r34jp.mongodb.net/${MONGO_DB}`,
+  mongodb_url: `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@todo-backend-cluster.by5ic.mongodb.net/${MONGO_DB}`,
 };
